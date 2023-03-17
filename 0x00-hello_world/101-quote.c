176d1516d8a98eac5c4b114cb283939ee30b199c
bhamzaoui <hamzaoui.bensaid@gmail.com>
2023-03-16 19:46:22 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <unistd.h>
 
+/**
+ * main - prints exactly "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+ *
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
 char *abc = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, abc, 59);
-return 1;
+write(STDERR_FILENO, abc, 59);
+return (1);
 }
+
