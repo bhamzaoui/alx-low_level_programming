b645f43dc8fb6c3db86fd8c0ab17cc2131f11f59
bhamzaoui <hamzaoui.bensaid@gmail.com>
2023-03-16 19:47:36 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,8 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+ * main - prints exactly "and that piece of art is useful\" 
+ * - Dora Korpar, 2015-10-19"
  *
  * Return: Always 1 (Error)
  */
